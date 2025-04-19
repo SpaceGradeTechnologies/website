ec09b38accb3b99a3651b17b05ee14ceb6b29909
SiddarthJb <siddarthjb04@gmail.com>
2025-04-20 01:27:41 +0530

    fixes css problem

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./index.css";
 
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
